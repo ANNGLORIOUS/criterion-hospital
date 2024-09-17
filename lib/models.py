fade4cbfa6e8687cce4be77d945bad2522b9a2de
ANNGLORIOUS <mueniannglorious@gmail.com>
2024-09-17 20:35:48 +0300

    appointment table is created

--- lib/models.py
+++ lib/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, PrimaryKeyConstraint
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey,  DateTime
 from sqlalchemy.orm import sessionmaker, relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import func
@@ -75,4 +75,36 @@ class Doctor(Base):
     def find_by_id(session, doctor_id):
         return session.query(Doctor).get(doctor_id)
 
-   
+class Appointment(Base):
+    __tablename__ = 'appointments'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    patient_id = Column(Integer, ForeignKey('patients.id'), nullable=False)
+    doctor_id = Column(Integer, ForeignKey('doctors.id'), nullable=False)
+    date = Column(DateTime, nullable=False)
+    patient = relationship("Patient", back_populates="appointments")
+    doctor = relationship("Doctor", back_populates="appointments")
+
+    def __repr__(self):
+        return (f"<Appointment(id={self.id}, patient_id={self.patient_id}, "
+                f"doctor_id={self.doctor_id}, date={self.date})>")
+
+    @staticmethod
+    def create(patient_id, doctor_id, date):
+        return Appointment(patient_id=patient_id, doctor_id=doctor_id, date=date)
+
+    @staticmethod
+    def delete(session, appointment_id):
+        appointment = session.query(Appointment).get(appointment_id)
+        if appointment:
+            session.delete(appointment)
+            session.commit()
+        return appointment
+
+    @staticmethod
+    def get_all(session):
+        return session.query(Appointment).all()
+
+    @staticmethod
+    def find_by_id(session, appointment_id):
+        return session.query(Appointment).get(appointment_id)  
